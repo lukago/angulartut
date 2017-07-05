7735cdeb976051adfe2421d83c36142b5f4be1bb
glbwsk <golebiewski96@gmail.com>
2017-07-05 23:50:18 +0200

    styles

--- quickstart/src/app/app.module.ts
+++ quickstart/src/app/app.module.ts
@@ -2,13 +2,13 @@ import {NgModule}       from '@angular/core';
 import {BrowserModule}  from '@angular/platform-browser';
 import {FormsModule}    from '@angular/forms';
 
-import {AppComponent}         from './app.component';
-import {HeroDetailComponent}  from './hero-detail.component'
-import {HeroesComponent}      from './heroes.component';
+import {AppComponent}         from './components/app.component';
+import {HeroDetailComponent}  from './components/hero-detail.component';
+import {HeroesComponent}      from './components/heroes.component';
 import {HeroService}          from './hero.service';
-import {DashboardComponent}   from './dasboard.component';
+import {DashboardComponent}   from './components/dasboard.component';
 
-import {AppRoutingModule}     from './app-routing.module'
+import {AppRoutingModule}     from './app-routing.module';
 
 
 @NgModule({
--- quickstart/src/app/components/dasboard.component.ts
+++ quickstart/src/app/components/dasboard.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 
-import {HeroService}  from './hero.service';
-import {Hero}         from './hero';
+import {HeroService}  from '../hero.service';
+import {Hero}         from '../hero';
 
 @Component({
   selector: 'my-dashboard',
-  templateUrl: './dashboard.component.html'
+  templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ]
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
